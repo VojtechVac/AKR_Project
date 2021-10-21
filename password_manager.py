4882d274a7a48130dcede8c7c9f8bbf10590283b
wondergrandma <oliverbielik@gmail.com>
2021-10-21 19:54:14 +0200

    gui pre vloženie mena a hesla (podla videa čo poslal Michal)

--- password_manager.py
+++ password_manager.py
@@ -1,3 +1,32 @@
-import hashlib
+from tkinter import *
 
 #pod toto pôjde kód
+
+window = Tk()
+
+window.title("Password Manager")
+
+def loginScreen():
+    window.geometry("640x360")
+    okno = Label(window, text = "Vlož email")
+    okno.config(anchor=CENTER)
+    okno.pack()
+
+    text = Entry(window, width = 20)
+    text.pack()
+
+    okno = Label(window, text = "Vlož heslo")
+    okno.config(anchor=CENTER)
+    okno.pack()
+
+    text = Entry(window, width = 20)
+    text.pack()
+
+    def kontrolaHesla():
+            print("test")
+
+    tlacidlo = Button(window, text = "Potvrdiť", command = kontrolaHesla)
+    tlacidlo.pack()
+    
+loginScreen()
+window.mainloop()
